bf105cb255da865e2a0f75bf3df0fe44b1b6cb93
Phelthas <luxiaoming@luxiaomingdeiMac.local>
2015-05-05 09:38:21 +0800

    add annotation

--- TEST_Common/Two/UIView+XMLPanGesture.h
+++ TEST_Common/Two/UIView+XMLPanGesture.h
@@ -31,6 +31,7 @@
 
 /**
  *  注意这个方法必须放在view被add到subView上之后调用,因为里面会用到view.superView
+ *  在iOS8的6或者6+上，这个方法需放到viewDidAppear里面，因为动画是改frame的，而layout在viewDidAppear的时候才会确定正确的frame
  */
 - (void)addPanGestureWithEndFrame:(CGRect)endFrame;
 
