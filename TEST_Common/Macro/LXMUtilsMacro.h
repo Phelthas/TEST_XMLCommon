22ff6070d0a48ef57d318e1bea10d09db3014f74
Phelthas <luxiaoming@luxiaomingdeiMac.local>
2015-06-29 16:31:07 +0800

    update macro

--- TEST_Common/Macro/LXMUtilsMacro.h
+++ TEST_Common/Macro/LXMUtilsMacro.h
@@ -18,12 +18,17 @@
 #define kLXMScreenHeight            [[UIScreen mainScreen] bounds].size.height
 #define kLXMScreenWidth             [[UIScreen mainScreen] bounds].size.width
 
-//判断屏幕尺寸
+//判断屏幕尺寸，注意这个取出来的是屏幕实际尺寸，iPad需要另外写
 #define is3_5Inch CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(640, 960))
 #define is4_0Inch CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(640, 1136))
 #define is4_7Inch CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(750, 1334))
 #define is5_5Inch CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(1242, 2208))
 
+//判读运行模式，判读程序运行时实际的“点”，iPad运行iPhone程序也可以用这个
+#define is320_480 CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(320, 480))
+#define is320_568 CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(320, 568))
+#define is375_667 CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(375, 667))
+#define is414_736 CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(414, 736))
 
 //颜色相关
 #define LXMColor(R, G, B, A)       [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
