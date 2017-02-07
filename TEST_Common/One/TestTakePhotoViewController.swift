1e4775edd50c3f0e552090cca85bbf86cb42302d
Phelthas <billthas@gmail.com>
2017-02-07 22:15:05 +0800

    update to swift3.0

--- TEST_Common/One/TestTakePhotoViewController.swift
+++ TEST_Common/One/TestTakePhotoViewController.swift
@@ -23,9 +23,9 @@ class TestTakePhotoViewController: UIViewController {
     var frontCameraDevice: AVCaptureDevice?
     var isBackCamera: Bool = true
     var previewLayer: AVCaptureVideoPreviewLayer!
-    var flashMode: AVCaptureFlashMode = AVCaptureFlashMode.Auto
+    var flashMode: AVCaptureFlashMode = AVCaptureFlashMode.auto
     var authorizationStatus: AVAuthorizationStatus {
-        return AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+        return AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
     }
     
     var currentCameraDevice: AVCaptureDevice? {
@@ -49,12 +49,12 @@ class TestTakePhotoViewController: UIViewController {
  
     }
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         startSession()
     }
     
-    override func viewWillDisappear(animated: Bool) {
+    override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         stopSession()
     }
@@ -69,16 +69,16 @@ class TestTakePhotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if context == &myContext && keyPath == "capturingStillImage" {
             if let dict = change,
-            let value = dict["new"] as? Bool where value == true {
+            let value = dict[NSKeyValueChangeKey.newKey] as? Bool, value == true {
                 self.stopSession()
             } else {
                 self.startSession()
             }
         } else {
-            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
     }
 
@@ -87,39 +87,39 @@ class TestTakePhotoViewController: UIViewController {
 // MARK: - PrivateMethod
 extension TestTakePhotoViewController {
     
-    private func setupTakePhotoView() {
-        takePhotoView.userInteractionEnabled = true
-        let changeFocusGesture = UITapGestureRecognizer(target: self, action: "handleChangeFocusGesture:")
+    fileprivate func setupTakePhotoView() {
+        takePhotoView.isUserInteractionEnabled = true
+        let changeFocusGesture = UITapGestureRecognizer(target: self, action: #selector(TestTakePhotoViewController.handleChangeFocusGesture(_:)))
         takePhotoView.addGestureRecognizer(changeFocusGesture)
     }
     
-    private func setupCamera() {
-        self.stillImageOutput.addObserver(self, forKeyPath: "capturingStillImage", options: .New, context: &myContext)
+    fileprivate func setupCamera() {
+        self.stillImageOutput.addObserver(self, forKeyPath: "capturingStillImage", options: .new, context: &myContext)
         
         
         switch authorizationStatus {
-        case.NotDetermined:
-            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: { (granted: Bool) -> Void in
+        case.notDetermined:
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { (granted: Bool) -> Void in
                 if granted {
                     self.configureCamera()
                 } else {
                     self.showErrorAlertView()
                 }
             })
-        case.Authorized:
+        case.authorized:
             self.configureCamera()
         default:
             self.showErrorAlertView()
         }
     }
     
-    private func configureCamera() {
+    fileprivate func configureCamera() {
         session.sessionPreset = AVCaptureSessionPresetPhoto
-        let availableCameraArray = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        let availableCameraArray = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo)
         for tempDevice in availableCameraArray as! [AVCaptureDevice] {
-            if tempDevice.position == .Back {
+            if tempDevice.position == .back {
                 backCameraDevice = tempDevice
-            } else if tempDevice.position == .Front {
+            } else if tempDevice.position == .front {
                 frontCameraDevice = tempDevice
             }
         }
@@ -152,11 +152,11 @@ extension TestTakePhotoViewController {
                 if tempDevice.hasFlash {
                     tempDevice.flashMode = self.flashMode
                 }
-                if tempDevice.isFocusModeSupported(.AutoFocus) {
-                    tempDevice.focusMode = .AutoFocus
+                if tempDevice.isFocusModeSupported(.autoFocus) {
+                    tempDevice.focusMode = .autoFocus
                 }
-                if tempDevice.isWhiteBalanceModeSupported(.AutoWhiteBalance) {
-                    tempDevice.whiteBalanceMode = .AutoWhiteBalance
+                if tempDevice.isWhiteBalanceModeSupported(.autoWhiteBalance) {
+                    tempDevice.whiteBalanceMode = .autoWhiteBalance
                 }
                 tempDevice.unlockForConfiguration()
                 
@@ -169,19 +169,19 @@ extension TestTakePhotoViewController {
         self.startSession()
     }
     
-    private func showErrorAlertView() {
+    fileprivate func showErrorAlertView() {
         let alert = UIAlertView(title: "提醒", message: "未授权应用使用相机，请前往设置内打开相机权限", delegate: nil, cancelButtonTitle: "确定")
         alert.show()
     }
     
-    private func startSession() { //如果有需要，可能需要另开线程来做
-        if authorizationStatus == .Authorized {
+    fileprivate func startSession() { //如果有需要，可能需要另开线程来做
+        if authorizationStatus == .authorized {
             session.startRunning()
         }
     }
     
-    private func stopSession() { //如果有需要，可能需要另开线程来做
-        if authorizationStatus == .Authorized {
+    fileprivate func stopSession() { //如果有需要，可能需要另开线程来做
+        if authorizationStatus == .authorized {
             session.stopRunning()
         }
     }
@@ -190,25 +190,27 @@ extension TestTakePhotoViewController {
 // MARK: - Action
 extension TestTakePhotoViewController {
     
-    func image(image: UIImage, didFinishSavingWithError: NSError?, contextInfo: AnyObject) {
+    func image(_ image: UIImage, didFinishSavingWithError: NSError?, contextInfo: AnyObject) {
         NSLog("error is \(didFinishSavingWithError)")
     }
 
     
     
     
-    @IBAction func handleTakePhotoButtonTapped(sender: UIButton) {
-        let connection = self.stillImageOutput.connectionWithMediaType(AVMediaTypeVideo)
-        self.stillImageOutput.captureStillImageAsynchronouslyFromConnection(connection) { [unowned self] (imageDataSampleBuffer: CMSampleBuffer!, error) -> Void in
+    @IBAction func handleTakePhotoButtonTapped(_ sender: UIButton) {
+        let connection = self.stillImageOutput.connection(withMediaType: AVMediaTypeVideo)
+        self.stillImageOutput.captureStillImageAsynchronously(from: connection) { [unowned self] (imageDataSampleBuffer, error) -> Void in
             if error == nil {
                 // 如果使用 session .Photo 预设，或者在设备输出设置中明确进行了设置,就能获得已经压缩为JPEG的数据
-                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-                // 样本缓冲区也包含元数据
-                // let metadata:NSDictionary = CMCopyDictionaryOfAttachments(nil, imageDataSampleBuffer, CMAttachmentMode(kCMAttachmentMode_ShouldPropagate))!
-                if let image = UIImage(data: imageData) {
+                if let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer),
+                    let image = UIImage(data: imageData) {
+                    // 样本缓冲区也包含元数据
+                    // let metadata:NSDictionary = CMCopyDictionaryOfAttachments(nil, imageDataSampleBuffer, CMAttachmentMode(kCMAttachmentMode_ShouldPropagate))!
+                    
                     //注意这个image还是摄像头拍下来时的分辨率，并不是你设置的layer大小的，如果还需要剪裁，就剪裁之后在保存
-                    UIImageWriteToSavedPhotosAlbum(image, self, "image:didFinishSavingWithError:contextInfo:", nil)
+                    UIImageWriteToSavedPhotosAlbum(image, self, #selector(TestTakePhotoViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
                 }
+                
             } else {
                 NSLog("error while capturing still image: \(error)")
             }
@@ -218,23 +220,23 @@ extension TestTakePhotoViewController {
         
     }
     
-    @IBAction func handleChangeCameraButtonTapped(sender: UIButton) {
+    @IBAction func handleChangeCameraButtonTapped(_ sender: UIButton) {
         self.isBackCamera = !self.isBackCamera
         self.configureCamera()
     }
     
-    @IBAction func handleFlashButtonTapped(sender: UIButton) {
+    @IBAction func handleFlashButtonTapped(_ sender: UIButton) {
         if let currentCameraDevice = currentCameraDevice {
             if currentCameraDevice.hasFlash == true {
                 try! currentCameraDevice.lockForConfiguration()
-                if currentCameraDevice.flashMode == .Off {
-                    currentCameraDevice.flashMode = .On;
+                if currentCameraDevice.flashMode == .off {
+                    currentCameraDevice.flashMode = .on;
                     
-                } else if currentCameraDevice.flashMode == .On {
-                    currentCameraDevice.flashMode = .Auto;
+                } else if currentCameraDevice.flashMode == .on {
+                    currentCameraDevice.flashMode = .auto;
                     
-                } else if currentCameraDevice.flashMode == .Auto {
-                    currentCameraDevice.flashMode = .Off;
+                } else if currentCameraDevice.flashMode == .auto {
+                    currentCameraDevice.flashMode = .off;
                     
                 }
                 currentCameraDevice.unlockForConfiguration()
@@ -243,26 +245,26 @@ extension TestTakePhotoViewController {
         
     }
     
-    @IBAction func handleBackButtonTapped(sender: UIButton) {
-        self.dismissViewControllerAnimated(true) { () -> Void in
+    @IBAction func handleBackButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true) { () -> Void in
             
         }
     }
     
-    @IBAction func handleChangeFocusGesture(sender: UITapGestureRecognizer) {
-        let pointInPreview = sender.locationInView(sender.view)
+    @IBAction func handleChangeFocusGesture(_ sender: UITapGestureRecognizer) {
+        let pointInPreview = sender.location(in: sender.view)
         if let tempLayer = self.previewLayer {
-            let pointInCamera = tempLayer.captureDevicePointOfInterestForPoint(pointInPreview)
+            let pointInCamera = tempLayer.captureDevicePointOfInterest(for: pointInPreview)
             if let currentCameraDevice = currentCameraDevice {
                 try! currentCameraDevice.lockForConfiguration()
-                if currentCameraDevice.focusPointOfInterestSupported == true {
+                if currentCameraDevice.isFocusPointOfInterestSupported == true {
                     currentCameraDevice.focusPointOfInterest = pointInCamera
-                    currentCameraDevice.focusMode = .AutoFocus
+                    currentCameraDevice.focusMode = .autoFocus
                 }
-                if currentCameraDevice.isExposureModeSupported(.AutoExpose) {
-                    if currentCameraDevice.exposurePointOfInterestSupported == true {
+                if currentCameraDevice.isExposureModeSupported(.autoExpose) {
+                    if currentCameraDevice.isExposurePointOfInterestSupported == true {
                         currentCameraDevice.exposurePointOfInterest = pointInCamera
-                        currentCameraDevice.exposureMode = .AutoExpose
+                        currentCameraDevice.exposureMode = .autoExpose
                     }
                 }
                 currentCameraDevice.unlockForConfiguration()
